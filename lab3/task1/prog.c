6bc583b48872b2a053eebff3a6540b46ffdf3fa5
SageInPocket <53127411+Mudrets@users.noreply.github.com>
2021-02-22 22:08:00 +0300

    Lab 6 done.

--- lab3/task1/prog.c
+++
@@ -1,8 +0,0 @@
-#include <sys/types.h>
-#include <unistd.h>
-#include <stdio.h>
-
-int main() {
-	printf("Process Id: %d\n", (int)getpid());
-	printf("Parent's process Id: %d\n", (int)getppid());
-}
